8f68946000c935a2d46373f43c7d2487f7cea879
Johnna Sundberg <johnna@sparkmicrogrants.org>
2017-05-04 12:28:24 +0300

    Updating Security Settings

--- __openerp__.py
+++ __openerp__.py
@@ -24,7 +24,7 @@
 
     # always loaded
     'data': [
-        # 'security/ir.model.access.csv',
+        'security/ir.model.access.csv',
         'views/facilitator_weekly_reports_views.xml',
     ],
     # only loaded in demonstration mode
